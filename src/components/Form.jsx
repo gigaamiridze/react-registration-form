ac5663107986e38538c5a7b7f23cfdf9378b1bf7
Giga Amiridze <flameyungoxx@gmail.com>
2022-12-11 20:08:31 +0400

    Added form functionality

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import logo from '../assets/logo.png';
 import saly from '../assets/saly.png';
 
 function Form() {
+  const { handleSubmit, register } = useForm();
+  const onSubmit = formData => alert(JSON.stringify(formData));
+
   return (
     <>
       <div className='form-block'>
@@ -10,30 +14,30 @@ function Form() {
           <img src={logo} className='logo' alt="Logo" />
           <h3>register</h3>
           <p>It's not long before you embark on this journey!</p>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className='form-group'>
               <label htmlFor="email">E-mail</label>
               <div className='input-box'>
-                <input type="email" placeholder='name@email.com' name='email' id='email' />
+                <input type="email" {...register('email')} placeholder='name@email.com' name='email' id='email' />
                 <i className='fa-regular fa-at'></i>
               </div>
             </div>
             <div className='form-group'>
               <label htmlFor="username">Username</label>
               <div className='input-box'>
-                <input type="text" placeholder='gigaamiridze' name='username' id='username' />
+                <input type="text" {...register('username')} placeholder='gigaamiridze' name='username' id='username' />
                 <i className='fa-regular fa-user'></i>
               </div>
             </div>
             <div className='form-group'>
               <label htmlFor="password">Password</label>
               <div className='input-box'>
-                <input type="password" placeholder='8+ Characters, 1 Capital letter' name='password' id='password' />
+                <input type="password" {...register('password')} placeholder='8+ Characters, 1 Capital letter' name='password' id='password' />
                 <i className='fa-solid fa-lock'></i>
               </div>
             </div>
             <div className='form-group'>
-              <input type="checkbox" name="agree" id="agree" />
+              <input type="checkbox" {...register('agree')} name="agree" id="agree" />
               <label htmlFor="agree" className='agree-label'>
                 I've read and agree with <span>Terms of Service</span> and our <span>Privacy Police</span>
               </label>
