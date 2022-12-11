1241cbea7964f3b68a75aee85e77ba696a3c2d65
Giga Amiridze <flameyungoxx@gmail.com>
2022-12-11 20:16:41 +0400

    Added validations of input fields

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo.png';
 import saly from '../assets/saly.png';
 
 function Form() {
-  const { handleSubmit, register } = useForm();
+  const { handleSubmit, register, formState: { errors } } = useForm();
   const onSubmit = formData => alert(JSON.stringify(formData));
 
   return (
@@ -18,21 +18,27 @@ function Form() {
             <div className='form-group'>
               <label htmlFor="email">E-mail</label>
               <div className='input-box'>
-                <input type="email" {...register('email')} placeholder='name@email.com' name='email' id='email' />
+                <input type="email" {...register('email', { required: true })} placeholder='name@email.com' name='email' id='email' />
+                {errors.email?.type === 'required' && <span className='error'>Email field is required</span>}
                 <i className='fa-regular fa-at'></i>
               </div>
             </div>
             <div className='form-group'>
               <label htmlFor="username">Username</label>
               <div className='input-box'>
-                <input type="text" {...register('username')} placeholder='gigaamiridze' name='username' id='username' />
+                <input type="text" {...register('username', { required: true, minLength: 5, maxLength: 20 })} placeholder='gigaamiridze' name='username' id='username' />
+                {errors.username?.type === 'required' && <span className='error left-pos'>Username field is required</span>}
+                {errors.username?.type === 'minLength' && <span className='error left-pos'>Minimum 5 characters required</span>}
+                {errors.username?.type === 'maxLength' && <span className='error left-pos'>Maximum 20 characters required</span>}
                 <i className='fa-regular fa-user'></i>
               </div>
             </div>
             <div className='form-group'>
               <label htmlFor="password">Password</label>
               <div className='input-box'>
-                <input type="password" {...register('password')} placeholder='8+ Characters, 1 Capital letter' name='password' id='password' />
+                <input type="password" {...register('password', { required: true, minLength: 8 })} placeholder='8+ Characters, 1 Capital letter' name='password' id='password' />
+                {errors.password?.type === 'required' && <span className='error left-pos'>Password field is required</span>}
+                {errors.password?.type === 'minLength' && <span className='error left-pos'>Minimum 8 characters required</span>}
                 <i className='fa-solid fa-lock'></i>
               </div>
             </div>
